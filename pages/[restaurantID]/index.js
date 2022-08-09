956285ae29f84da162a6873cefc85445d5cd3a0e
Shuyang Ji <seanji2002@gmail.com>
2022-08-09 18:03:15 -0400

    Fixed Details Bug on Remote Server

--- pages/[restaurantID]/index.js
+++ pages/[restaurantID]/index.js
@@ -31,30 +31,30 @@ function RestaurantDetails(props) {
   );
 }
 
-export async function getStaticPaths() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://creater-jsy:test123@cluster0.mhkhd.mongodb.net/?retryWrites=true&w=majority"
-  );
+// export async function getStaticPaths() {
+//   const client = await MongoClient.connect(
+//     "mongodb+srv://creater-jsy:test123@cluster0.mhkhd.mongodb.net/?retryWrites=true&w=majority"
+//   );
 
-  const db = client.db();
+//   const db = client.db();
 
-  const restaurantsCollection = db.collection("restaurants");
+//   const restaurantsCollection = db.collection("restaurants");
 
-  const restaurants = await restaurantsCollection
-    .find({}, { _id: 1 })
-    .toArray();
+//   const restaurants = await restaurantsCollection
+//     .find({}, { _id: 1 })
+//     .toArray();
 
-  client.close();
+//   client.close();
 
-  return {
-    fallback: false,
-    paths: restaurants.map((restaurant) => ({
-      params: { restaurantID: restaurant._id.toString() },
-    })),
-  };
-}
+//   return {
+//     fallback: false,
+//     paths: restaurants.map((restaurant) => ({
+//       params: { restaurantID: restaurant._id.toString() },
+//     })),
+//   };
+// }
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   // fetch data for a single restaurant
   const restaurantID = context.params.restaurantID;
 
@@ -66,11 +66,34 @@ export async function getStaticProps(context) {
 
   const restaurantsCollection = db.collection("restaurants");
 
+  // check that the id is a string of 24 hex characters
+  if (!ObjectId.isValid(restaurantID)) {
+    // if no restaurant found, return 404
+    context.res.statusCode = 404;
+    context.res.end();
+    return {
+      props: {
+        error: 'invalid id',
+      }
+    };
+  }
+
   const selectedRestaurant = await restaurantsCollection.findOne({
     _id: ObjectId(restaurantID),
   });
 
   client.close();
+
+  if (!selectedRestaurant) {
+    // if no restaurant found, return 404
+    context.res.statusCode = 404;
+    context.res.end();
+    return {
+      props: {
+        error: 'id not found',
+      }
+    };
+  }
 
   return {
     props: {
